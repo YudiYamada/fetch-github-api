f65c01e6cefa69d6c56770fee48b58d1f4de8aaa
Yudi Yamada <yudiyamada10@gmail.com>
2024-12-17 17:24:11 -0300

    adicionando busca pela tecla enter

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,16 @@ document.getElementById("btn-search").addEventListener("click", () => {
     getUserProfile(userName)
 })
 
+document.getElementById("input-search").addEventListener("keyup", (e) => {
+    const userName = e.target.value
+    const key = e.which || e.keyCode
+    const isEnterKeyPressed = key === 13
+
+    if(isEnterKeyPressed){
+        getUserProfile(userName)
+    }
+})
+
 async function user(userName){
     const response = await fetch(`https://api.github.com/users/${userName}`)
     return await response.json()
