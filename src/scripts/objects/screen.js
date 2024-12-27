53a2fe6cc05661d714afe875c8e0e1a8a0fc3aab
Yudi Yamada <yudiyamada10@gmail.com>
2024-12-27 16:03:42 -0300

    adicionando informações dos últimos eventos feitos pelo usuário

--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -7,26 +7,38 @@ const screen = {
             <div class="data">
                 <h1>${user.name ?? "Não possui nome cadastrado 😢"}</h1>
                 <p>${user.bio ?? "Não possui bio cadastrada 😓"}</p>
-                <p>Seguindores ${user.followers }</p>
-                <p>Seguindo ${user.following }</p>
+                <p>Seguidores ${user.followers}</p>
+                <p>Seguindo ${user.following}</p>
             </div>
-        </div>`
+        </div>`;
 
-        let repositoriesItens = ""
+        let repositoriesItens = "";
         user.repositories.forEach(repo => repositoriesItens += `
-            <li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`)
+            <li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`);
 
-            if(user.repositories.length > 0){
-                this.userProfile.innerHTML += `
-                <div class="repositories section">
-                    <h2>Repositórios</h2>
-                    <ul>${repositoriesItens}</ul>
-                </div>`
-            }
+        if (user.repositories.length > 0) {
+            this.userProfile.innerHTML += `
+            <div class="repositories section">
+                <h2>Repositórios</h2>
+                <ul>${repositoriesItens}</ul>
+            </div>`;
+        }
+
+        let eventsItems = "";
+        user.events.forEach(event => eventsItems += `
+            <li>${event.type} - ${new Date(event.created_at).toLocaleString()}</li>`);
+
+        if (user.events.length > 0) {
+            this.userProfile.innerHTML += `
+            <div class="events section">
+                <h2>Eventos Recentes</h2>
+                <ul>${eventsItems}</ul>
+            </div>`;
+        }
     },
-    renderNotFound(){
-        this.userProfile.innerHTML = "<h3>Usuário não encontrado</h3>"
+    renderNotFound() {
+        this.userProfile.innerHTML = "<h3>Usuário não encontrado</h3>";
     }
-}
+};
 
-export { screen }
+export { screen };
---
+++ src/scripts/services/events.js
@@ -0,0 +1,12 @@
+import { baseUrl, eventsQuantity } from "/src/scripts/variables.js"
+
+async function getUserEvents(userName) {
+    const response = await fetch(`${baseUrl}/${userName}/events?per_page=${eventsQuantity}`);
+    const events = await response.json();
+
+    const filteredEvents = events.filter(event => event.type === 'CreateEvent' || event.type === 'PushEvent');
+    
+    return filteredEvents;
+}
+
+export { getUserEvents };
